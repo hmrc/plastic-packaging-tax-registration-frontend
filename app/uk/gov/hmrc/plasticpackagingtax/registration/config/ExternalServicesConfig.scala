19c43b1a5b701b5c9f2801eb64a642c5d76abf99
Tomasz Bartosiewicz <2415677+tombart@users.noreply.github.com>
2021-01-11 17:23:11 +0000

    PPTP-278: initial start page (TBC)

---
+++ app/uk/gov/hmrc/plasticpackagingtax/registration/config/ExternalServicesConfig.scala
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.plasticpackagingtax.registration.config
+
+import com.google.inject.{Inject, Singleton}
+import play.api.Configuration
+
+@Singleton
+class ExternalServicesConfig @Inject()(val configuration: Configuration) {
+
+  private def loadUrl(key: String): String =
+    configuration.getOptional[String](s"urls.$key").getOrElse(throw new Exception(s"Missing configuration key: urls.$key"))
+
+  val serviceAvailabilityUrl: String = loadUrl("serviceAvailability")
+  val govUkUrl: String = loadUrl("govUk")
+  val organisationsUrl: String = loadUrl("organisationsLink")
+  val explorePpt: String = loadUrl("explorePpt")
+}
