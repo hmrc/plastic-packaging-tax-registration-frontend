9fec29a637914c3146e5a242293fe1cbca383640
Tomasz Bartosiewicz <2415677+tombart@users.noreply.github.com>
2021-01-12 12:39:10 +0000

    PPTP-278: added UT skeleton + initial test

--- app/uk/gov/hmrc/plasticpackagingtax/registration/controllers/StartController.scala
+++ app/uk/gov/hmrc/plasticpackagingtax/registration/controllers/StartController.scala
@@ -16,6 +16,7 @@
 
 package uk.gov.hmrc.plasticpackagingtax.registration.controllers
 
+import play.api.i18n.I18nSupport
 import play.api.mvc._
 import uk.gov.hmrc.plasticpackagingtax.registration.config.AppConfig
 import uk.gov.hmrc.plasticpackagingtax.registration.views.html.start_page
@@ -26,12 +27,9 @@ import scala.concurrent.Future
 
 @Singleton
 class StartController @Inject()(
-  appConfig: AppConfig,
   mcc: MessagesControllerComponents,
   startPage: start_page
-) extends FrontendController(mcc) {
-
-  implicit val config: AppConfig = appConfig
+) extends FrontendController(mcc) with I18nSupport {
 
   val displayStartPage: Action[AnyContent] = Action.async { implicit request =>
     Future.successful(Ok(startPage()))
