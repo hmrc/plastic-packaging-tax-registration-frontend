cd161d44797561862dbd9afc4d0b3b5562008fdb
Tomasz Bartosiewicz <2415677+tombart@users.noreply.github.com>
2021-06-01 09:45:18 +0100

    PPTP-833: email ver

--- app/uk/gov/hmrc/plasticpackagingtax/registration/models/emailverification/EmailVerificationService.scala
+++ app/uk/gov/hmrc/plasticpackagingtax/registration/models/emailverification/EmailVerificationService.scala
@@ -6,20 +6,24 @@ import uk.gov.hmrc.plasticpackagingtax.registration.connectors.EmailVerification
 class EmailVerificationService(emailVerificationConnector: EmailVerificationConnector) {
 
   def isVerified(email: String, emailStatuses: Seq[EmailStatus]): Boolean = {
-    false
+    val ea = Map("email@gmail.com" -> Status(true, false))
+
+    return ea.get("email@gmail.com").filter(s => s.verified).getOrElse(false)
   }
 
   def createEmailVerification()(implicit hc: HeaderCarrier) =
-    emailVerificationConnector.create(CreateEmailVerificationRequest(credId = "credId",
-    continueUrl = "http://continue",
-    origin = "origin",
-    accessibilityStatementUrl = "http://accessibility",
-    email = Email(address = "test@hmrc.com",
-      enterUrl = "hhtp://enterUrl"
-    ),
-    backUrl = "http://back",
-    pageTitle = "PPT Title",
-    deskproServiceName = "ppt"
-  ))
+    emailVerificationConnector.create(
+      CreateEmailVerificationRequest(credId = "credId",
+                                     continueUrl = "http://continue",
+                                     origin = "origin",
+                                     accessibilityStatementUrl = "http://accessibility",
+                                     email = Email(address = "test@hmrc.com",
+                                                   enterUrl = "hhtp://enterUrl"
+                                     ),
+                                     backUrl = "http://back",
+                                     pageTitle = "PPT Title",
+                                     deskproServiceName = "ppt"
+      )
+    )
 
 }
