21cc56b86d654d283916ec54bc4026afb6eee7a6
Ryan Kelly <ryan.kelly@digital.hmrc.gov.uk>
2021-12-22 17:03:21 +0000

    PPTP-1178 : Revert unnecessary Tampermonkey version change

--- tampermonkey/PPT_AutoComplete.js
+++ tampermonkey/PPT_AutoComplete.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         PPT Registration AutoComplete
 // @namespace    http://tampermonkey.net/
-// @version      15.3
+// @version      15.2
 // @description
 // @author       pmonteiro
 // @match        http*://*/register-for-plastic-packaging-tax*
