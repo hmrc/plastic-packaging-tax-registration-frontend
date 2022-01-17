4049ae571661d35d52d7174748e85534694ad8a2
Tony McCrae <tony.mccrae@digital.hmrc.gov.uk>
2022-01-17 16:51:08 +0000

    PPTP-1550 Test coverage backfill.

---
+++ test/uk/gov/hmrc/plasticpackagingtax/registration/controllers/partnership/PartnershipPartnersPhoneNumberControllerSpec.scala
@@ -0,0 +1,117 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.plasticpackagingtax.registration.controllers.partnership
+
+import base.unit.ControllerSpec
+import org.mockito.ArgumentMatchers.any
+import org.mockito.Mockito.{reset, when}
+import org.mockito.stubbing.OngoingStubbing
+import org.scalatest.matchers.must.Matchers.convertToAnyMustWrapper
+import play.api.http.Status.{NOT_FOUND, OK}
+import play.api.libs.json.Json
+import play.api.test.DefaultAwaitTimeout
+import play.api.test.Helpers.status
+import play.twirl.api.HtmlFormat
+import uk.gov.hmrc.plasticpackagingtax.registration.connectors.{
+  DownstreamServiceError,
+  ServiceError
+}
+import uk.gov.hmrc.plasticpackagingtax.registration.forms.contact.{EmailAddress, PhoneNumber}
+import uk.gov.hmrc.plasticpackagingtax.registration.models.emailverification.{
+  CreateEmailVerificationRequest,
+  VerificationStatus
+}
+import uk.gov.hmrc.plasticpackagingtax.registration.models.genericregistration.OtherPartner
+import uk.gov.hmrc.plasticpackagingtax.registration.views.html.partnerships.phone_number_page
+import uk.gov.hmrc.play.bootstrap.tools.Stubs.stubMessagesControllerComponents
+
+import scala.concurrent.Future
+
+class PartnershipPartnersPhoneNumberControllerSpec extends ControllerSpec with DefaultAwaitTimeout {
+
+  private val page = mock[phone_number_page]
+  private val mcc  = stubMessagesControllerComponents()
+
+  private val controller =
+    new PartnershipOtherPartnerPhoneNumberController(authenticate = mockAuthAction,
+                                                     journeyAction = mockJourneyAction,
+                                                     registrationConnector =
+                                                       mockRegistrationConnector,
+                                                     mcc = mcc,
+                                                     page = page
+    )
+
+  override protected def beforeEach(): Unit = {
+    super.beforeEach()
+    when(page.apply(any(), any(), any(), any())(any(), any())).thenReturn(HtmlFormat.empty)
+  }
+
+  override protected def afterEach(): Unit = {
+    reset(page)
+    super.afterEach()
+  }
+
+  def registrationWithInflightOtherPartnerJourney =
+    aRegistration(withPartnershipDetails(Some(generalPartnershipDetails))).withInflightOtherPartner(
+      Some(OtherPartner(firstName = Some("John"), lastName = Some("Smith")))
+    )
+
+  "PartnershipOtherPartnerPhoneNumberController" should {
+
+    "return 200" when {
+      "user is authorised, a registration already exists with already collected contact name and display page method is invoked" in {
+        authorizedUser()
+        mockRegistrationFind(registrationWithInflightOtherPartnerJourney)
+
+        val result = controller.displayPage()(getRequest())
+
+        status(result) mustBe OK
+      }
+    }
+
+    "return 404" when {
+      "user is authorised but does not have an inflight journey and display page method is invoked" in {
+        authorizedUser()
+
+        val result = controller.displayPage()(getRequest())
+
+        status(result) mustBe NOT_FOUND
+      }
+    }
+
+    "return an error" when {
+
+      "user is not authorised" in {
+        unAuthorizedUser()
+
+        val result = controller.displayPage()(getRequest())
+
+        intercept[RuntimeException](status(result))
+      }
+
+      "user submits form and the registration update fails" in {
+        authorizedUser()
+        mockRegistrationUpdateFailure()
+        val result =
+          controller.submit()(postRequest(Json.toJson(PhoneNumber("12345678"))))
+
+        intercept[DownstreamServiceError](status(result))
+      }
+    }
+  }
+
+}
