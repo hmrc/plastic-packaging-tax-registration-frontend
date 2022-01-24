732b38e335496f1a7506e2979a5555f955dd1444
Tony McCrae <tony.mccrae@digital.hmrc.gov.uk>
2022-01-24 15:20:28 +0000

    PPTP-1550 Spike - use amend by id pattern to ammend contact name of existing partner.

--- app/uk/gov/hmrc/plasticpackagingtax/registration/controllers/partner/PartnerContactNameController.scala
+++ app/uk/gov/hmrc/plasticpackagingtax/registration/controllers/partner/PartnerContactNameController.scala
@@ -73,6 +73,32 @@ class PartnerContactNameController @Inject() (
       }.getOrElse(throw new IllegalStateException("Expected partner missing"))
     }
 
+  def displayExistingPartner(partnerId: String): Action[AnyContent] =
+    (authenticate andThen journeyAction) { implicit request =>
+      request.registration.findPartner(partnerId).map { partner =>
+        val existingNameFields = for {
+          contactDetails <- partner.contactDetails
+          firstName      <- contactDetails.firstName
+          lastName       <- contactDetails.lastName
+        } yield (firstName, lastName)
+
+        val form = existingNameFields match {
+          case Some(data) =>
+            MemberName.form().fill(MemberName(data._1, data._2))
+          case None =>
+            MemberName.form()
+        }
+
+        Ok(
+          page(form,
+               partner.name,
+               partnerRoutes.PartnerCheckAnswersController.displayExistingPartner(partnerId),
+               partnerRoutes.PartnerContactNameController.submitExistingPartner(partnerId)
+          )
+        )
+      }.getOrElse(throw new IllegalStateException("Expected existing partner missing"))
+    }
+
   def submit(): Action[AnyContent] =
     (authenticate andThen journeyAction).async { implicit request =>
       request.registration.inflightPartner.map { partner =>
@@ -90,7 +116,7 @@ class PartnerContactNameController @Inject() (
                 )
               ),
             fullName =>
-              updateRegistration(fullName).map {
+              updateInflightPartner(fullName).map {
                 case Right(_) =>
                   FormAction.bindFromRequest match {
                     case SaveAndContinue =>
@@ -106,7 +132,44 @@ class PartnerContactNameController @Inject() (
       )
     }
 
-  private def updateRegistration(
+  def submitExistingPartner(partnerId: String): Action[AnyContent] =
+    (authenticate andThen journeyAction).async { implicit request =>
+      request.registration.findPartner(partnerId).map { partner =>
+        MemberName.form()
+          .bindFromRequest()
+          .fold(
+            (formWithErrors: Form[MemberName]) =>
+              Future.successful(
+                BadRequest(
+                  page(formWithErrors,
+                       partner.name,
+                       partnerRoutes.NominatedPartnerTypeController.displayPage(),
+                       partnerRoutes.PartnerContactNameController.submit()
+                  )
+                )
+              ),
+            fullName =>
+              updateExistingPartner(fullName, partnerId).map {
+                case Right(_) =>
+                  FormAction.bindFromRequest match {
+                    case SaveAndContinue =>
+                      Redirect(
+                        routes.PartnerCheckAnswersController.displayExistingPartner(partnerId)
+                      )
+                    case _ =>
+                      Redirect(
+                        partnerRoutes.PartnerContactNameController.displayExistingPartner(partnerId)
+                      )
+                  }
+                case Left(error) => throw error
+              }
+          )
+      }.getOrElse(
+        Future.successful(throw new IllegalStateException("Expected existing partner missing"))
+      )
+    }
+
+  private def updateInflightPartner(
     formData: MemberName
   )(implicit req: JourneyRequest[AnyContent]): Future[Either[ServiceError, Registration]] =
     update { registration =>
@@ -122,6 +185,22 @@ class PartnerContactNameController @Inject() (
       }.getOrElse {
         registration
       }
+    }
+
+  private def updateExistingPartner(formData: MemberName, partnerId: String)(implicit
+    req: JourneyRequest[AnyContent]
+  ): Future[Either[ServiceError, Registration]] =
+    update { registration =>
+      registration.withUpdatedPartner(partnerId,
+                                      partner =>
+                                        partner.copy(contactDetails =
+                                          partner.contactDetails.map(
+                                            _.copy(firstName = Some(formData.firstName),
+                                                   lastName = Some(formData.lastName)
+                                            )
+                                          )
+                                        )
+      )
     }
 
 }
