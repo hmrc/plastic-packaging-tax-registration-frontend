7caf3f810534ed99389f5b6684046ebeda76f042
Ryan Kelly <ryan.kelly@digital.hmrc.gov.uk>
2022-02-11 16:45:28 +0000

    PPTP-1424 : Post Reg - Remove Partner

--- app/uk/gov/hmrc/plasticpackagingtax/registration/controllers/amendment/partner/ConfirmRemovePartnerController.scala
+++ app/uk/gov/hmrc/plasticpackagingtax/registration/controllers/amendment/partner/ConfirmRemovePartnerController.scala
@@ -16,16 +16,21 @@
 
 package uk.gov.hmrc.plasticpackagingtax.registration.controllers.amendment.partner
 
+import play.api.data.Form
 import play.api.i18n.I18nSupport
-import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents, Result}
 import uk.gov.hmrc.plasticpackagingtax.registration.controllers.actions.AuthNoEnrolmentCheckAction
 import uk.gov.hmrc.plasticpackagingtax.registration.controllers.amendment.AmendmentController
-import uk.gov.hmrc.plasticpackagingtax.registration.controllers.group.RemoveMemberAction
-import uk.gov.hmrc.plasticpackagingtax.registration.models.request.AmendmentJourneyAction
+import uk.gov.hmrc.plasticpackagingtax.registration.forms.partner.RemovePartner
+import uk.gov.hmrc.plasticpackagingtax.registration.models.registration.Registration
+import uk.gov.hmrc.plasticpackagingtax.registration.models.request.{
+  AmendmentJourneyAction,
+  JourneyRequest
+}
 import uk.gov.hmrc.plasticpackagingtax.registration.views.html.amendment.partner.confirm_remove_partner_page
 
 import javax.inject.{Inject, Singleton}
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
 class ConfirmRemovePartnerController @Inject() (
@@ -34,17 +39,56 @@ class ConfirmRemovePartnerController @Inject() (
   mcc: MessagesControllerComponents,
   page: confirm_remove_partner_page
 )(implicit ec: ExecutionContext)
-    extends AmendmentController(mcc, amendmentJourneyAction) with I18nSupport
-    with RemoveMemberAction {
+    extends AmendmentController(mcc, amendmentJourneyAction) with I18nSupport {
 
   def displayPage(partnerId: String): Action[AnyContent] =
     (authenticate andThen amendmentJourneyAction) { implicit request =>
-      Ok(page())
+      Ok(page(RemovePartner.form(), getPartner(partnerId, request.registration)))
     }
 
-  def submit(memberId: String): Action[AnyContent] =
-    (authenticate andThen amendmentJourneyAction) { implicit request =>
-      Redirect(routes.PartnersListController.displayPage())
+  private def getPartner(partnerId: String, registration: Registration) =
+    registration.findPartner(partnerId).getOrElse(
+      throw new IllegalStateException("Specified partner not found")
+    )
+
+  def submit(partnerId: String): Action[AnyContent] =
+    (authenticate andThen amendmentJourneyAction).async { implicit request =>
+      request.registration.findPartner(partnerId).map { partner =>
+        RemovePartner.form().bindFromRequest().fold(
+          { formWithErrors: Form[RemovePartner] =>
+            Future.successful(BadRequest(page(formWithErrors, partner)))
+          },
+          { removePartner: RemovePartner =>
+            removePartner.value match {
+              case Some(true) =>
+                doRemovePartner(partner.id)
+              case _ =>
+                Future.successful(Redirect(routes.PartnersListController.displayPage()))
+            }
+          }
+        )
+      }.getOrElse {
+        throw new IllegalStateException("Could not find partner")
+      }
     }
+
+  private def doRemovePartner(
+    partnerId: String
+  )(implicit req: JourneyRequest[AnyContent]): Future[Result] =
+    updateRegistration(
+      registration =>
+        registration.copy(organisationDetails =
+          registration.organisationDetails.copy(partnershipDetails =
+            registration.organisationDetails.partnershipDetails.map(
+              _.copy(partners =
+                registration.organisationDetails.partnershipDetails.map(
+                  _.partners.filter(_.id != partnerId)
+                ).getOrElse(Seq())
+              )
+            )
+          )
+        ),
+      routes.PartnersListController.displayPage()
+    )
 
 }
