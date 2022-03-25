78463858abe1800c569c976d04af2498026beb81
Ryan Kelly <ryan.kelly@digital.hmrc.gov.uk>
2022-03-25 11:26:21 +0000

    PPTP-1880 : Deregistration Skeleton - Phase III
    Ensure that the deregistration reasons are consistent with definition at the back end.

--- app/uk/gov/hmrc/plasticpackagingtax/registration/models/deregistration/DeregistrationDetails.scala
+++ app/uk/gov/hmrc/plasticpackagingtax/registration/models/deregistration/DeregistrationDetails.scala
@@ -22,8 +22,10 @@ import uk.gov.hmrc.plasticpackagingtax.registration.models.deregistration.Deregi
 object DeregistrationReason extends Enumeration {
   type DeregistrationReason = Value
 
-  val RegisteredIncorrectly, CeasedTrading, NotMetAndDoNotExpectToMeetThreshold,
-    WantToRegisterAsGroup = Value
+  val RegisteredIncorrectly               = Value("Registered Incorrectly")
+  val CeasedTrading                       = Value("Ceased Trading")
+  val NotMetAndDoNotExpectToMeetThreshold = Value("Below De-minimus")
+  val WantToRegisterAsGroup               = Value("Taken into Group Registration")
 
   implicit val format: Format[DeregistrationReason] =
     Format(Reads.enumNameReads(DeregistrationReason), Writes.enumNameWrites)
