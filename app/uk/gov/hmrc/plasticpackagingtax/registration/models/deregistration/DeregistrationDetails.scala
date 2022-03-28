a358c969d4cf6f6a6fdc8b0769d63302caa7b35b
Paul Blake <32386617+paulblakehmrc@users.noreply.github.com>
2022-03-28 17:24:30 +0100

    PPTP-1890: deregistration reason

--- app/uk/gov/hmrc/plasticpackagingtax/registration/models/deregistration/DeregistrationDetails.scala
+++ app/uk/gov/hmrc/plasticpackagingtax/registration/models/deregistration/DeregistrationDetails.scala
@@ -19,19 +19,6 @@ package uk.gov.hmrc.plasticpackagingtax.registration.models.deregistration
 import play.api.libs.json._
 import uk.gov.hmrc.plasticpackagingtax.registration.models.deregistration.DeregistrationReason.DeregistrationReason
 
-object DeregistrationReason extends Enumeration {
-  type DeregistrationReason = Value
-
-  val RegisteredIncorrectly               = Value("Registered Incorrectly")
-  val CeasedTrading                       = Value("Ceased Trading")
-  val NotMetAndDoNotExpectToMeetThreshold = Value("Below De-minimus")
-  val WantToRegisterAsGroup               = Value("Taken into Group Registration")
-
-  implicit val format: Format[DeregistrationReason] =
-    Format(Reads.enumNameReads(DeregistrationReason), Writes.enumNameWrites)
-
-}
-
 case class DeregistrationDetails(deregister: Option[Boolean], reason: Option[DeregistrationReason])
 
 object DeregistrationDetails {
