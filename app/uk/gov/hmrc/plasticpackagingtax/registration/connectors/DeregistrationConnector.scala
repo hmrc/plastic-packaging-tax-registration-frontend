c59ce61612ca7c16e1203d0cc4f2859d7b14bb91
kiranprakashdigital <74252533+kiranprakashdigital@users.noreply.github.com>
2022-03-29 18:13:21 +0100

    PPTP-1787 Check your answers- Deregistration page

---
+++ app/uk/gov/hmrc/plasticpackagingtax/registration/connectors/DeregistrationConnector.scala
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.plasticpackagingtax.registration.connectors
+
+import com.kenshoo.play.metrics.Metrics
+import play.api.http.Status.OK
+import uk.gov.hmrc.http.HttpReads.Implicits._
+import uk.gov.hmrc.http.{HeaderCarrier, HttpClient, HttpResponse}
+import uk.gov.hmrc.plasticpackagingtax.registration.config.AppConfig
+import uk.gov.hmrc.plasticpackagingtax.registration.models.deregistration.DeregistrationDetails
+import uk.gov.hmrc.plasticpackagingtax.registration.models.deregistration.DeregistrationReason.DeregistrationReason
+
+import javax.inject.{Inject, Singleton}
+import scala.concurrent.{ExecutionContext, Future}
+
+@Singleton
+class DeregistrationConnector @Inject() (
+  httpClient: HttpClient,
+  appConfig: AppConfig,
+  metrics: Metrics
+)(implicit ec: ExecutionContext) {
+
+  def deregister(pptReference: String, deregistrationDetails: DeregistrationDetails)(implicit
+    hc: HeaderCarrier
+  ): Future[Either[ServiceError, HttpResponse]] = {
+    val timer = metrics.defaultRegistry.timer("ppt.deregister.timer").time()
+    httpClient.PUT[Option[DeregistrationReason], HttpResponse](
+      appConfig.pptSubscriptionDeregisterUrl(pptReference),
+      deregistrationDetails.reason
+    ).andThen { case _ => timer.stop() }
+      .map {
+        case response @ HttpResponse(OK, _, _) =>
+          Right(response)
+        case response =>
+          Left(
+            DownstreamServiceError(
+              s"Failed to de-register, status: ${response.status}, error: ${response.body}",
+              FailedToDeregister("Failed to de-register")
+            )
+          )
+      }.recover {
+        case ex: Exception =>
+          Left(DownstreamServiceError(s"Error while de-registration, error: ${ex.getMessage}", ex))
+      }
+  }
+
+}
+
+case class FailedToDeregister(errorMessage: String) extends Exception
---
+++ test/uk/gov/hmrc/plasticpackagingtax/registration/connectors/DeregistrationConnectorISpec.scala
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.plasticpackagingtax.registration.connectors
+
+import base.it.ConnectorISpec
+import com.github.tomakehurst.wiremock.client.WireMock.{aResponse, put}
+import org.scalatest.EitherValues
+import org.scalatest.concurrent.ScalaFutures
+import play.api.http.Status
+import play.api.test.Helpers.{await, OK}
+import play.api.test.Injecting
+import uk.gov.hmrc.http.HttpResponse
+import uk.gov.hmrc.plasticpackagingtax.registration.models.deregistration.{
+  DeregistrationDetails,
+  DeregistrationReason
+}
+
+class DeregistrationConnectorISpec
+    extends ConnectorISpec with Injecting with ScalaFutures with EitherValues {
+
+  lazy val connector: DeregistrationConnector =
+    inject[DeregistrationConnector]
+
+  val deregistrationDetails = DeregistrationDetails(deregister = Some(true),
+                                                    reason =
+                                                      Some(DeregistrationReason.CeasedTrading)
+  )
+
+  val pptReference = "XMPPT123456789"
+
+  "Deregister" should {
+    "return success response" when {
+
+      "valid request is sent" in {
+
+        givenPutDeregistration(pptReference, Status.OK, DeregistrationReason.CeasedTrading.toString)
+
+        val res: Either[ServiceError, HttpResponse] =
+          await(connector.deregister(pptReference, deregistrationDetails))
+
+        res.right.get.status mustBe OK
+        getTimer("ppt.deregister.timer").getCount mustBe 1
+      }
+
+    }
+
+    "return error " when {
+
+      "service returns non success status code" in {
+        givenPutDeregistration(pptReference,
+                               Status.BAD_REQUEST,
+                               DeregistrationReason.CeasedTrading.toString
+        )
+
+        val res: Either[ServiceError, HttpResponse] =
+          await(connector.deregister(pptReference, deregistrationDetails))
+
+        res.left.value.getMessage must include("Failed to de-register")
+
+        res.left.value.isInstanceOf[DownstreamServiceError]
+      }
+
+      "service returns invalid response" in {
+        givenPutDeregistration(pptReference, Status.BAD_REQUEST, "something")
+
+        val res = await(connector.deregister(pptReference, deregistrationDetails))
+
+        res.left.value.isInstanceOf[DownstreamServiceError]
+      }
+    }
+
+  }
+
+  private def givenPutDeregistration(pptReference: String, status: Int, body: String) =
+    stubFor(
+      put(s"/subscriptions/deregister/${pptReference}")
+        .willReturn(
+          aResponse()
+            .withStatus(status)
+            .withBody(body)
+        )
+    )
+
+}
