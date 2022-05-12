dfa15c03708079268782124a219ee00e462cb2a9
ajchilts <40767309+ajchilts@users.noreply.github.com>
2022-05-12 16:30:21 +0100

    Fix spec

--- test/uk/gov/hmrc/plasticpackagingtax/registration/views/organisation/PartnershipPartnerTypeViewSpec.scala
+++ test/uk/gov/hmrc/plasticpackagingtax/registration/views/organisation/PartnershipPartnerTypeViewSpec.scala
@@ -155,7 +155,7 @@ class PartnershipPartnerTypeViewSpec extends UnitViewSpec with Matchers {
       radioInputMustBe(4, LIMITED_LIABILITY_PARTNERSHIP)
       radioInputMustBe(5, SCOTTISH_PARTNERSHIP)
       radioInputMustBe(6, SCOTTISH_LIMITED_PARTNERSHIP)
-      radioInputMustBe(8, OVERSEAS_COMPANY_UK_BRANCH)
+      radioInputMustBe(7, OVERSEAS_COMPANY_UK_BRANCH)
     }
 
     "display 'Save and continue' button" in {
