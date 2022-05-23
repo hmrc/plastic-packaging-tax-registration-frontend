784a50f8135b8274fb37ec823c049dcef1aa9dd8
“lmcewanhmrc” <lindsay.mcewan@digital.hmrc.gov.uk>
2022-05-23 14:26:45 +0100

    wip - make other grs callback like partners grs callback

--- app/uk/gov/hmrc/plasticpackagingtax/registration/controllers/organisation/GrsController.scala
+++ app/uk/gov/hmrc/plasticpackagingtax/registration/controllers/organisation/GrsController.scala
@@ -18,25 +18,17 @@ package uk.gov.hmrc.plasticpackagingtax.registration.controllers.organisation
 
 import play.api.Logger
 import play.api.i18n.I18nSupport
-import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import play.api.mvc.{Action, AnyContent, Call, MessagesControllerComponents}
 import uk.gov.hmrc.http.HttpReads.Implicits._
 import uk.gov.hmrc.http.{HeaderCarrier, InternalServerException}
 import uk.gov.hmrc.plasticpackagingtax.registration.connectors._
 import uk.gov.hmrc.plasticpackagingtax.registration.connectors.grs._
 import uk.gov.hmrc.plasticpackagingtax.registration.controllers.actions.NotEnrolledAuthAction
 import uk.gov.hmrc.plasticpackagingtax.registration.controllers.group.{routes => groupRoutes}
-import uk.gov.hmrc.plasticpackagingtax.registration.controllers.organisation.RegistrationStatus.{
-  BUSINESS_VERIFICATION_FAILED,
-  DUPLICATE_SUBSCRIPTION,
-  GRS_FAILED,
-  RegistrationStatus,
-  SOLE_TRADER_VERIFICATION_FAILED,
-  STATUS_OK,
-  UNSUPPORTED_ORGANISATION
-}
+import uk.gov.hmrc.plasticpackagingtax.registration.controllers.organisation.RegistrationStatus.{BUSINESS_VERIFICATION_FAILED, DUPLICATE_SUBSCRIPTION, GRS_FAILED, RegistrationStatus, SOLE_TRADER_VERIFICATION_FAILED, STATUS_OK, UNSUPPORTED_ORGANISATION}
 import uk.gov.hmrc.plasticpackagingtax.registration.controllers.organisation.{routes => orgRoutes}
 import uk.gov.hmrc.plasticpackagingtax.registration.controllers.{routes => commonRoutes}
-import uk.gov.hmrc.plasticpackagingtax.registration.forms.organisation.OrgType
+import uk.gov.hmrc.plasticpackagingtax.registration.forms.organisation.{OrgType, PartnerTypeEnum}
 import uk.gov.hmrc.plasticpackagingtax.registration.forms.organisation.OrgType.SOLE_TRADER
 import uk.gov.hmrc.plasticpackagingtax.registration.forms.organisation.PartnerTypeEnum.PartnerTypeEnum
 import uk.gov.hmrc.plasticpackagingtax.registration.models.genericregistration._
@@ -91,50 +83,74 @@ class GrsController @Inject() (
                   s"and details [${registration.organisationDetails.grsRegistration.getOrElse("None")}]"
               )
               status match {
-                case STATUS_OK =>
+                case STATUS_OK => {
                   Redirect(orgRoutes.ConfirmBusinessAddressController.displayPage())
-                case GRS_FAILED =>
+                }
+                case GRS_FAILED => {
                   throw new Exception(s"Unexpected response from GRS during journey-id $journeyId")
-                case BUSINESS_VERIFICATION_FAILED =>
+                }
+                case BUSINESS_VERIFICATION_FAILED => {
                   Redirect(commonRoutes.NotableErrorController.businessVerificationFailure())
-                case SOLE_TRADER_VERIFICATION_FAILED =>
+                }
+                case SOLE_TRADER_VERIFICATION_FAILED => {
                   Redirect(commonRoutes.NotableErrorController.soleTraderVerificationFailure())
-                case DUPLICATE_SUBSCRIPTION =>
+                }
+                case DUPLICATE_SUBSCRIPTION => {
                   if (registration.isGroup)
                     Redirect(groupRoutes.NotableErrorController.nominatedOrganisationAlreadyRegistered())
                   else
                     Redirect(commonRoutes.NotableErrorController.duplicateRegistration())
-                case UNSUPPORTED_ORGANISATION =>
+                }
+                case UNSUPPORTED_ORGANISATION => {
                   Redirect(orgRoutes.RegisterAsOtherOrganisationController.onPageLoad())
+                }
               }
             }
           case Left(error) => throw error
         }
     }
 
-  private def registrationStatus(
-    registration: Registration
-  )(implicit hc: HeaderCarrier, request: JourneyRequest[AnyContent]): Future[RegistrationStatus] =
-    if (registration.organisationDetails.businessVerificationFailed)
+  private def registrationStatus(registration: Registration)
+    (implicit hc: HeaderCarrier, request: JourneyRequest[AnyContent]): Future[Call] = {
+
+    // Note - business-verification may be disabled
+    val nextPage: Call = if (registration.organisationDetails.businessVerificationFailed) {
       registration.organisationDetails.organisationType match {
-        case Some(SOLE_TRADER) =>
-          Future.successful(SOLE_TRADER_VERIFICATION_FAILED)
-        case _ =>
-          Future.successful(BUSINESS_VERIFICATION_FAILED)
+        case PartnerTypeEnum.SOLE_TRADER
+          | PartnerTypeEnum.SCOTTISH_PARTNERSHIP
+          | PartnerTypeEnum.GENERAL_PARTNERSHIP
+          | PartnerTypeEnum.LIMITED_LIABILITY_PARTNERSHIP
+          | PartnerTypeEnum.SCOTTISH_LIMITED_PARTNERSHIP => {
+          commonRoutes.NotableErrorController.soleTraderVerificationFailure()
+        }
+        case _ => commonRoutes.NotableErrorController.businessVerificationFailure()
       }
-    else
-      registration.organisationDetails.businessPartnerId match {
-        case Some(businessPartnerId) =>
-          checkSubscriptionStatus(businessPartnerId, registration).map {
-            case SUBSCRIBED => DUPLICATE_SUBSCRIPTION
-            case _          => STATUS_OK
-          }
-        case None =>
-          Future.successful(registration.organisationDetails.grsRegistration match {
-            case Some(reg) if !reg.identifiersMatch => SOLE_TRADER_VERIFICATION_FAILED //todo stand in for check your details
-            case _ => GRS_FAILED
-          })
+
+      Future.successful(nextPage)
+    }
+
+    else {
+
+    registration.organisationDetails.businessPartnerId match {
+
+      // Success - we got a business-partner-id
+      case Some(businessPartnerId) => {
+        checkSubscriptionStatus(businessPartnerId, registration).map {
+          case SUBSCRIBED => DUPLICATE_SUBSCRIPTION
+          case _ => STATUS_OK
+        }
       }
+
+      // Something wrong
+      case None => {
+        Future.successful(registration.organisationDetails.grsRegistration match {
+          case Some(reg) if !reg.identifiersMatch => SOLE_TRADER_VERIFICATION_FAILED //todo stand in for check your details
+          case _ => GRS_FAILED
+        })
+      }
+    }
+    }
+  }
 
   private def checkSubscriptionStatus(businessPartnerId: String, registration: Registration)(implicit
     hc: HeaderCarrier,
@@ -158,8 +174,9 @@ class GrsController @Inject() (
     journeyId: String
   )(implicit hc: HeaderCarrier, request: JourneyRequest[AnyContent]): Future[Either[ServiceError, Registration]] = {
     request.registration.organisationDetails.organisationType match {
-      case Some(OrgType.UK_COMPANY) | Some(OrgType.OVERSEAS_COMPANY_UK_BRANCH) =>
+      case Some(OrgType.UK_COMPANY) | Some(OrgType.OVERSEAS_COMPANY_UK_BRANCH) => {
         updateUkCompanyDetails(journeyId)
+      }
       case Some(OrgType.REGISTERED_SOCIETY) => updateRegisteredSocietyDetails(journeyId)
       case Some(OrgType.SOLE_TRADER)        => updateSoleTraderDetails(journeyId)
       case Some(OrgType.PARTNERSHIP)        => updatePartnershipDetails(journeyId)
@@ -204,7 +221,7 @@ class GrsController @Inject() (
 
   private def updatePartnershipDetails(journeyId: String)(implicit request: JourneyRequest[AnyContent]): Future[Registration] =
     request.registration.organisationDetails.partnershipDetails match {
-      case Some(partnershipDetails) =>
+      case Some(partnershipDetails) => {
         partnershipGrsConnector.getDetails(journeyId).map { partnershipBusinessDetails =>
           request.registration.copy(
             incorpJourneyId = Some(journeyId),
@@ -216,6 +233,7 @@ class GrsController @Inject() (
             )
           )
         }
+      }
       case _ => throw new IllegalStateException("Missing partnership details")
     }
 
