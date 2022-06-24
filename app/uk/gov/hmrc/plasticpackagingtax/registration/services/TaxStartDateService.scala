7f1c03541019b26b68cdd2f9cf4585d8118de9ef
“lmcewanhmrc” <lindsay.mcewan@digital.hmrc.gov.uk>
2022-06-24 22:26:58 +0100

    Add some tests for unanswered question when calculating tax start date

--- app/uk/gov/hmrc/plasticpackagingtax/registration/services/TaxStartDateService.scala
+++ app/uk/gov/hmrc/plasticpackagingtax/registration/services/TaxStartDateService.scala
@@ -26,11 +26,34 @@ class TaxStartDateServiceImpl extends TaxStartDateService {
 
   //todo actually calculate
   def calculateTaxStartDate(details: LiabilityDetails): Option[LocalDate] = Some(LocalDate.of(1996, 3, 27))
-//    if (details.exceededThresholdWeight.contains(true))
-//      details.dateExceededThresholdWeight.map(o => calculateExceedStartDate(o))
-//    else if (details.expectToExceedThresholdWeight.contains(true))
-//      details.dateRealisedExpectedToExceedThresholdWeight.map(_.date)
-//    else Some(LocalDate.of(1996, 3, 27))
+  //    if (details.exceededThresholdWeight.contains(true))
+  //      details.dateExceededThresholdWeight.map(o => calculateExceedStartDate(o))
+  //    else if (details.expectToExceedThresholdWeight.contains(true))
+  //      details.dateRealisedExpectedToExceedThresholdWeight.map(_.date)
+  //    else Some(LocalDate.of(1996, 3, 27))
+
+
+  def taxStartDate(liabilityDetails: LiabilityDetails): Option[LocalDate] = {
+    
+    if (liabilityDetails.exceededThresholdWeight.isEmpty)
+      throw new IllegalStateException("Missing answer for 'exceededThresholdWeight'")
+    
+    if (liabilityDetails.dateExceededThresholdWeight.isEmpty && liabilityDetails.exceededThresholdWeight.get)
+      throw new IllegalStateException("Missing answer for 'dateExceededThresholdWeight'")
+
+    if (liabilityDetails.expectToExceedThresholdWeight.isEmpty)
+      throw new IllegalStateException("Missing answer for 'expectToExceedThresholdWeight'")
+      
+    if (liabilityDetails.dateRealisedExpectedToExceedThresholdWeight.isEmpty && liabilityDetails.expectToExceedThresholdWeight.get)
+      throw new IllegalStateException("Missing answer for 'dateRealisedExpectedToExceedThresholdWeight'")
+
+    if (liabilityDetails.exceededThresholdWeight.contains(true))
+      liabilityDetails.dateExceededThresholdWeight.map(o => calculateExceedStartDate(o))
+    else if (liabilityDetails.expectToExceedThresholdWeight.contains(true))
+      liabilityDetails.dateRealisedExpectedToExceedThresholdWeight.map(_.date)
+    else 
+      throw new IllegalStateException("huh?") 
+  }
 
   private def calculateExceedStartDate(date: Date): LocalDate =
     date.date.plusMonths(1).withDayOfMonth(1)
--- test/uk/gov/hmrc/plasticpackagingtax/registration/services/TaxStartDateServiceSpec.scala
+++ test/uk/gov/hmrc/plasticpackagingtax/registration/services/TaxStartDateServiceSpec.scala
@@ -18,52 +18,139 @@ package uk.gov.hmrc.plasticpackagingtax.registration.services
 
 import org.scalatestplus.play.PlaySpec
 import uk.gov.hmrc.plasticpackagingtax.registration.forms
+import uk.gov.hmrc.plasticpackagingtax.registration.forms.Date
+import uk.gov.hmrc.plasticpackagingtax.registration.forms.liability.LiabilityWeight
 import uk.gov.hmrc.plasticpackagingtax.registration.models.registration.LiabilityDetails
 
 import java.time.LocalDate
 
+class YesNoDate private(private val maybeDate: Option[LocalDate]) {
+  def fold[B](ifEmpty: => B)(f: LocalDate => B): B = maybeDate.fold(ifEmpty)(f)
+}
+
+object YesNoDate {
+  def yesAndDate(date: LocalDate) : YesNoDate = new YesNoDate(Some(date))
+  def no() : YesNoDate = new YesNoDate(None)
+}
+
 class TaxStartDateServiceSpec extends PlaySpec {
 
   private val dateExceededThresholdWeight = LocalDate.of(2022, 4, 30)
+  private val taxStartDateService = new TaxStartDateServiceImpl
+
+  private val completedLiabilityDetails = LiabilityDetails(
+    exceededThresholdWeight = Some(true), 
+    dateExceededThresholdWeight = Some(Date(LocalDate.of(2022, 4, 1))),   
+    expectToExceedThresholdWeight = Some(true), 
+    dateRealisedExpectedToExceedThresholdWeight = Some(Date(LocalDate.of(2022, 4, 1))),
+    expectedWeightNext12m = Some(LiabilityWeight(Some(20000L)))
+  )
+
+  "Unanswered questions" should {
+    "cause an error" when {
+
+      "all answers are missing" in {
+        an [IllegalStateException] must be thrownBy {
+          taxStartDateService.taxStartDate(LiabilityDetails())
+        }
+      }
+
+      "the backwards yes-no answer is missing" in {
+        val missingBackwardsYesNo = completedLiabilityDetails.copy(exceededThresholdWeight = None)
+        the [IllegalStateException] thrownBy {
+          taxStartDateService.taxStartDate(missingBackwardsYesNo) 
+        } must have message "Missing answer for 'exceededThresholdWeight'"
+      }
+
+      "the forwards yes-no answer is missing" in {
+        val missingForwardsYesNo = completedLiabilityDetails.copy(expectToExceedThresholdWeight = None)
+        the [IllegalStateException] thrownBy {
+          taxStartDateService.taxStartDate(missingForwardsYesNo)
+        } must have message "Missing answer for 'expectToExceedThresholdWeight'"
+      }
+
+
+      "the forwards date answer is missing" when {
+
+        val missingForwardsDate = completedLiabilityDetails.copy(
+          expectToExceedThresholdWeight = Some(true),
+          dateRealisedExpectedToExceedThresholdWeight = None)
+        
+        "the forwards yes-no answer was yes" in  {
+          the [IllegalStateException] thrownBy {
+            taxStartDateService.taxStartDate(missingForwardsDate)
+          } must have message "Missing answer for 'dateRealisedExpectedToExceedThresholdWeight'"
+        }
+        
+        "but not when the forwards yes-no answer was no" in {
+          val missingForwardsDate2 = missingForwardsDate.copy(expectToExceedThresholdWeight = Some(false))
+          noException should be thrownBy taxStartDateService.taxStartDate(missingForwardsDate2)
+        }
+        
+      }
+      
+      
+      "the backwards date is missing" when {
+
+        val missingBackwardsDate = completedLiabilityDetails.copy(
+          exceededThresholdWeight = Some(true),
+          dateExceededThresholdWeight = None)
+        
+        "the backwards yes-no answer was yes" in {
+          the [IllegalStateException] thrownBy {
+            taxStartDateService.taxStartDate(missingBackwardsDate)
+          } must have message "Missing answer for 'dateExceededThresholdWeight'"
+        }
+        
+        "but not when the backwards yes-no answer was no" in {
+          val missingBackwardsDate2 = missingBackwardsDate.copy(exceededThresholdWeight = Some(false))
+          noException should be thrownBy taxStartDateService.taxStartDate(missingBackwardsDate2)
+        }
+        
+      }
+      
+    }
+  }
+
+  "Answering no to both questions" should {
+    "mean the user is not liable" ignore {
+    }
+  }
+
   "return 1st day of the month following the date capture" when {
     "plastic package limit are exceeded" in {
-      new TaxStartDateServiceImpl()
-        .calculateTaxStartDate(createLiabilityDetailsForBreachedThreshold) mustBe
-        Some(LocalDate.of(2022, 5, 1))
+      val liabilityDetailsForBreachedThreshold = LiabilityDetails(
+        expectToExceedThresholdWeight = Some(false), 
+        exceededThresholdWeight = Some(true),
+        dateExceededThresholdWeight = Some(forms.Date(dateExceededThresholdWeight))
+      )
+      taxStartDateService.taxStartDate(liabilityDetailsForBreachedThreshold) mustBe Some(LocalDate.of(2022, 5, 1))
     }
   }
 
   "return the capture date" when {
     "plastic package limit will be exceed in the next 30 days" in {
-      new TaxStartDateServiceImpl()
-        .calculateTaxStartDate(createLiabilityDetailsForThresholdWouldBeBreached) mustBe
-        Some(dateExceededThresholdWeight)
+      val liabilityDetailsForThresholdWouldBeBreached = LiabilityDetails(
+        expectToExceedThresholdWeight = Some(true),
+        dateRealisedExpectedToExceedThresholdWeight = Some(forms.Date(LocalDate.of(2022, 4, 30))),
+        exceededThresholdWeight = Some(false)
+      )
+      taxStartDateService.taxStartDate(liabilityDetailsForThresholdWouldBeBreached) mustBe Some(dateExceededThresholdWeight)
     }
   }
 
   "return an error" when {
-    "no exceeded threshold date is set" in {
-      new TaxStartDateServiceImpl()
-        .calculateTaxStartDate(LiabilityDetails(exceededThresholdWeight = Some(true))) mustBe None
+    "no exceeded threshold date is set" ignore {
+      taxStartDateService
+        .taxStartDate(LiabilityDetails(exceededThresholdWeight = Some(true))) mustBe None
     }
 
-    "no dateRealisedExpectedToExceedThresholdWeight is set" in {
-      new TaxStartDateServiceImpl()
-        .calculateTaxStartDate(
+    "no dateRealisedExpectedToExceedThresholdWeight is set" ignore {
+      taxStartDateService
+        .taxStartDate(
           LiabilityDetails(expectToExceedThresholdWeight = Some(true))
         ) mustBe None
     }
   }
-
-  private def createLiabilityDetailsForThresholdWouldBeBreached =
-    LiabilityDetails(expectToExceedThresholdWeight = Some(true),
-                     dateRealisedExpectedToExceedThresholdWeight =
-                       Some(forms.Date(LocalDate.of(2022, 4, 30)))
-    )
-
-  private def createLiabilityDetailsForBreachedThreshold =
-    LiabilityDetails(exceededThresholdWeight = Some(true),
-                     dateExceededThresholdWeight = Some(forms.Date(dateExceededThresholdWeight))
-    )
 
 }
