1620ee8de8b3747447c55e78fba24261f4bd83a9
“lmcewanhmrc” <lindsay.mcewan@digital.hmrc.gov.uk>
2022-06-28 17:58:32 +0100

    Bit of a tidy in TaxStartDateControllerSpec

--- test/uk/gov/hmrc/plasticpackagingtax/registration/controllers/liability/TaxStartDateControllerSpec.scala
+++ test/uk/gov/hmrc/plasticpackagingtax/registration/controllers/liability/TaxStartDateControllerSpec.scala
@@ -21,10 +21,9 @@ import org.mockito.ArgumentMatchers.{any, eq => eqq}
 import org.mockito.BDDMockito.`given`
 import org.mockito.Mockito.{reset, verify}
 import org.scalatest.matchers.must.Matchers.convertToAnyMustWrapper
-import play.api.http.Status.SEE_OTHER
 import play.api.libs.json.JsObject
 import play.api.mvc.Results.{Ok, Redirect}
-import play.api.test.Helpers.{await, redirectLocation, status}
+import play.api.test.Helpers.{await, status}
 import play.twirl.api.HtmlFormat
 import uk.gov.hmrc.plasticpackagingtax.registration.services.{TaxStartDate, TaxStartDateService}
 import uk.gov.hmrc.plasticpackagingtax.registration.views.html.liability.tax_start_date_page
@@ -40,17 +39,19 @@ class TaxStartDateControllerSpec extends ControllerSpec {
   private val aDate = LocalDate.of(2022, 4, 1)
   private val aRegistration = super.aRegistration()
 
-  val sut = new TaxStartDateController(mockAuthAction,
-                                       mockJourneyAction,
-                                       mockTaxStartDateService,
-                                       mcc,
-                                       page,
-                                       mockRegistrationConnector
+  val sut = new TaxStartDateController(
+    mockAuthAction,
+    mockJourneyAction,
+    mockTaxStartDateService,
+    mcc,
+    page,
+    mockRegistrationConnector
   )
 
   override protected def beforeEach(): Unit = {
     super.beforeEach()
     reset(page, mockTaxStartDateService, mockRegistrationConnector)
+    authoriseAndSetRegistration
   }
 
   private def authoriseAndSetRegistration = {
@@ -58,47 +59,43 @@ class TaxStartDateControllerSpec extends ControllerSpec {
     mockRegistrationFind(aRegistration())
   }
 
-  "Tax Start Date page " should {
+  "display page" should {
     
+    "pass the liability answers to the tax start date service" in {
+      given(mockTaxStartDateService.calculateTaxStartDate(any())).willReturn(TaxStartDate.notLiable)
+      await(sut.displayPage()(getRequest()))
+      verify(mockTaxStartDateService).calculateTaxStartDate(eqq(aRegistration.liabilityDetails))
+    }
+
     "bounce to the not liable page" in {
-      authoriseAndSetRegistration
       given(mockTaxStartDateService.calculateTaxStartDate(any())).willReturn(TaxStartDate.notLiable)
       val result = await(sut.displayPage()(getRequest()))
-      
-      verify(mockTaxStartDateService).calculateTaxStartDate(eqq(aRegistration.liabilityDetails))
       result mustBe Redirect(routes.NotLiableController.displayPage())
     }
 
     "display tax start date page" in {
-      authoriseAndSetRegistration
       given(mockTaxStartDateService.calculateTaxStartDate(any())).willReturn(TaxStartDate.liableFromForwardsTest(aDate))
       given(page.apply(any(), any())(any(), any())).willReturn(HtmlFormat.raw("tax start date blah"))
       val result = await(sut.displayPage()(getRequest()))
-      
-      verify(mockTaxStartDateService).calculateTaxStartDate(eqq(aRegistration.liabilityDetails))
-      verify(page).apply(eqq(aDate), eqq(false))(any(), any()) // TODO <--- tests for false vs true
+      verify(page).apply(eqq(aDate), eqq(false))(any(), any())
       result mustBe Ok(HtmlFormat.raw("tax start date blah"))
     }
-    
+
     "return an error" when {
       "user is not authorised and display page method is invoked" in {
         unAuthorizedUser()
         val result = sut.displayPage()(getRequest())
         intercept[RuntimeException](status(result))
       }
-
     }
+    
+  }
 
-    "submit" should {
-      "redirect to Capture Weight page" when {
-        "submit" in {
-          authoriseAndSetRegistration
-          val result = sut.submit()(postRequest(JsObject.empty))
-          status(result) mustBe SEE_OTHER
-          redirectLocation(result) mustBe Some(routes.LiabilityWeightController.displayPage().url)
-        }
-      }
-    }  
+  "submit" should {
+    "redirect to the next page" in {
+      val result = await(sut.submit()(postRequest(JsObject.empty)))
+      result mustBe Redirect(routes.LiabilityWeightController.displayPage())
+    }
   }
 
 }
