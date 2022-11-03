371ead5daf7d86a808deb426f9c5128d9c863c68
PSouthern <45816181+PSouthern@users.noreply.github.com>
2022-11-03 16:13:50 +0000

    Adding amendment view a11y specs

---
+++ a11y/registration/amendment/group/ManageGroupMembersPageA11ySpec.scala
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package registration.amendment.group
+
+import support.BaseViewSpec
+import uk.gov.hmrc.plasticpackagingtax.registration.config.AppConfig
+import uk.gov.hmrc.plasticpackagingtax.registration.forms.organisation.OrgType.UK_COMPANY
+import uk.gov.hmrc.plasticpackagingtax.registration.models.registration.{OrganisationDetails, Registration}
+import uk.gov.hmrc.plasticpackagingtax.registration.views.html.amendment.group.manage_group_members_page
+
+class ManageGroupMembersPageA11ySpec extends BaseViewSpec {
+
+  val page: manage_group_members_page = inject[manage_group_members_page]
+  val realAppConfig: AppConfig        = inject[AppConfig]
+
+  val registration: Registration = Registration("someID",
+                                                organisationDetails = OrganisationDetails(
+                                                  organisationType = Some(UK_COMPANY),
+                                                  incorporationDetails = Some(incorporationDetails)
+                                                ),
+                                                groupDetail = Some(
+                                                  groupDetails.copy(members =
+                                                    Seq(groupMember, groupMember)
+                                                  )
+                                                )
+  )
+
+  private def createView(): String =
+    page(registration)(journeyRequest, messages).toString()
+
+  val view = createView()
+
+  "manage_group_members_page" must {
+
+    "pass accessibility checks without error" in {
+      view must passAccessibilityChecks
+    }
+  }
+}
---
+++ a11y/registration/amendment/partner/AmendPartnerContactCheckAnswersViewSpec.scala
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package registration.amendment.partner
+
+import support.BaseViewSpec
+import uk.gov.hmrc.plasticpackagingtax.registration.views.html.amendment.partner.amend_partner_contact_check_answers_page
+import uk.gov.hmrc.plasticpackagingtax.registration.views.tags.ViewTest
+
+@ViewTest
+class AmendPartnerContactCheckAnswersViewSpec extends BaseViewSpec {
+
+  private val page = inject[amend_partner_contact_check_answers_page]
+
+  private val partner = aLimitedCompanyPartner()
+
+  "Amend Partner Contact Check Answers View" should {
+
+    val view: String = page(partner)(journeyRequest, messages).toString()
+
+    "pass accessibility checks without error" in {
+      view must passAccessibilityChecks
+    }
+  }
+}
