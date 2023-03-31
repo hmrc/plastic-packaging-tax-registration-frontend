b1a0aaf366ce0af5c4c96c07e868d714884901c4
mauro-fanton <89914846+mauro-fanton@users.noreply.github.com>
2023-03-31 10:36:06 +0100

    Revert "getting all passcode (#596)" (#598)
    This reverts commit a62997aa08ec698bbac57c5ff3e17201589aa249.

--- app/controllers/testOnly/EmailPasscodeController.scala
+++ app/controllers/testOnly/EmailPasscodeController.scala
@@ -21,11 +21,6 @@ import play.api.mvc._
 import connectors.testOnly.EmailTestOnlyPasscodeConnector
 import controllers.actions.PermissiveAuthActionImpl
 import models.request.JourneyAction
-import models.testOnly.EmailPasscode
-import play.api.{Environment, Mode}
-import play.api.libs.json.{JsArray, Json, OWrites}
-import repositories.testOnly.{JourneyMongoRepository, PasscodeMongoRepository}
-import uk.gov.hmrc.http.SessionId
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendController
 
 import javax.inject.Inject
@@ -35,9 +30,6 @@ class EmailPasscodeController @Inject() (
                                           authenticate: PermissiveAuthActionImpl,
                                           mcc: MessagesControllerComponents,
                                           journeyAction: JourneyAction,
-                                          journeyRepo: JourneyMongoRepository,
-                                          passCodeRep: PasscodeMongoRepository,
-                                          environment: Environment,
                                           emailTestOnlyPasscodeConnector: EmailTestOnlyPasscodeConnector
 )(implicit ec: ExecutionContext)
     extends FrontendController(mcc) with I18nSupport {
@@ -50,36 +42,4 @@ class EmailPasscodeController @Inject() (
       }
     }
 
-  def testOnlyGetAllPasscodes(): Action[AnyContent] =
-    authenticate.async { implicit request =>
-
-      if (isInProductionMode) throw new RuntimeException("Wrong environment")
-      else {
-
-        hc.sessionId match {
-          case Some(SessionId(id)) =>
-            for {
-              passcodeDocs <- passCodeRep.findPasscodesBySessionId(id)
-              journeyDocs <- journeyRepo.findAll
-              emailPasscodes = passcodeDocs.map(d => EmailPasscode(d.email, d.passcode)) ++
-                journeyDocs.filter(_.emailAddress.isDefined).map(d => EmailPasscode(d.emailAddress.get, d.passcode))
-            } yield Ok(Json.obj {
-              "passcodes" -> JsArray(emailPasscodes.map {
-                Json.toJson(_)
-              })
-            })
-          case None =>
-            Future.successful(Unauthorized(Json.toJson(ErrorResponse("NO_SESSION_ID", "No session id provided"))))
-        }
-      }
-    }
-
-  private def isInProductionMode = environment.mode == Mode.Prod
-
-}
-
-case class ErrorResponse(code: String, message: String, details: Option[Map[String, String]] = None)
-
-object ErrorResponse {
-  implicit val writes: OWrites[ErrorResponse] = Json.writes[ErrorResponse]
 }
