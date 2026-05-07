dd53980033893998c896e163f512751ca478d699
Dips <13121210+dips7189@users.noreply.github.com>
2026-05-07 09:13:44 +0100

    DDCYLS-7904 upgrading play frontend to latest giving us the service navigation header (#650)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -11,7 +11,7 @@ object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     ws,
     "uk.gov.hmrc"       %% s"bootstrap-frontend-$playVersion"            % bootstrapVersion,
-    "uk.gov.hmrc"       %% s"play-frontend-hmrc-$playVersion"            % "12.32.0",
+    "uk.gov.hmrc"       %% s"play-frontend-hmrc-$playVersion"            % "13.4.0",
     "uk.gov.hmrc.mongo" %% s"hmrc-mongo-$playVersion"                    % mongoVersion,
     "uk.gov.hmrc"       %% s"play-conditional-form-mapping-$playVersion" % "3.5.0"
   )
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -11,7 +11,7 @@ addSbtPlugin("uk.gov.hmrc" % "sbt-distributables" % "2.6.0")
 
 addSbtPlugin("org.playframework" % "sbt-plugin" % "3.0.10")
 
-addSbtPlugin("io.github.irundaia" % "sbt-sassify" % "1.5.2")
+addSbtPlugin("uk.gov.hmrc" % "sbt-sass-compiler" % "0.13.0")
 
 addSbtPlugin("org.scoverage" % "sbt-scoverage" % "2.4.2")
 
